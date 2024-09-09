eb2fdc3b490b8f8cce421d72da54cc713764f1fa
ChumanY <cuerpoespin_2000@hotmail.com>
2024-09-09 01:02:43 -0500

    d

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/decant" element={<Inicio />} />
+        <Route path="/" element={<Inicio />} />
         <Route path="/producto" element={<Producto />} />
         <Route path="/producto/premium" element={<Premium />} />
         <Route path="/producto/standard" element={<Standard />} />
@@ -28,7 +28,7 @@ function App() {
         <Route path="/sobre-nosotros" element={<SobreNosotros />} />
         <Route path="/servicios" element={<Servicios />} />
         {/* Catch-all route to redirect to homepage */}
-        <Route path="*" element={<Navigate to="/decant" />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
     </Router>
